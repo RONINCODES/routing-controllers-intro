381f64fbc5a5db34a7393685eea93d09eac86b75
Ronen Annason <ronenannason83@gmail.com>
2017-06-11 14:10:49 -0400

    set route for about view and contest view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
 class PagesController < ApplicationController
 
   def welcome
-  
+
 
   end
+
+  def about
+
+  end
+
+  def contest
+
+  end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get '/' => 'pages#welcome'
   get '/welcome' => 'pages#welcome'
+  get '/about'   => 'pages#about'
+  get '/contest' => 'pages#contest'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
